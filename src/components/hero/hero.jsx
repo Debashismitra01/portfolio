a58e7a1a5d71a9c18521eb7ff7922635d5b78a60
Debashis Mitra <146477651+Debashismitra01@users.noreply.github.com>
2024-05-27 17:06:40 +0530

    Update hero.jsx

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './hero.css';
 import profileImage from '../../assets/profile.jpg';
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
     return (
@@ -9,8 +10,12 @@ const Hero = () => {
             <h1>Hi,there </h1>
             <h2> I'M <span>DEBASHIS MITRA</span></h2>
             <div className="hero-action">
-                <div className="hero-connect">Connect with me</div>
-                <a className="hero-resume" href="https://drive.google.com/uc?export=download&id=1JFK06oc3LIk1tZjhll4CzoXlIgxlc0J6" download="DebashisMitraResume">My Resume</a>
+                <div className="hero-connect"><AnchorLink 
+                        offset={50}
+                        className="AnchorLink"
+                        href="#contact" 
+                    >Connect with me</AnchorLink></div>
+                <a className="hero-resume" href="https://drive.google.com/uc?export=download&id=1GrCA4ClFWKOFdbYOKs32qwQmuOvnONIi" download="DebashisMitraResume">My Resume</a>
             </div>
             </div>
             <div className="profile">
